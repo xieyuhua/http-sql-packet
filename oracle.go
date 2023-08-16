07e39ca80fc5af1eb449c31c81e16c02b65ab681
xieyuhua <1130107545@qq.com>
2023-08-16 16:58:41 +0800

    Update oracle.go

--- oracle.go
+++ oracle.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"log"
+// 	"log"
 	"strings"
+	"bytes"
 	"github.com/axgle/mahonia"
 )
+
 
 func fixChineseEncoding(input string) string {
     decoder := mahonia.NewDecoder("gbk")
@@ -14,28 +16,42 @@ func fixChineseEncoding(input string) string {
     return input
 }
 
+//BytesCombine 多个[]byte数组合并成一个[]byte
+func BytesCombine(pBytes ...[]byte) []byte {
+    return bytes.Join(pBytes, []byte(""))
+}
+
+
 // 解析TCP负载中的Oracle SQL语句
-func ParseOracleSQL(client, server string, payloadStr  []byte) string {
+func ParseOracleSQL(client, server string,n int, payloadStr []byte) string {
     
 	// 使用正则表达式匹配SQL语句
-	re := regexp.MustCompile(`(?i)\b(SELECT|INSERT|UPDATE|DELETE)\b.*`)
-// 	re := regexp.MustCompile(`(?i)\b(SELECT|INSERT|UPDATE|DELETE)\b.*?;`)
-
+// 	re := regexp.MustCompile(`(?i)\b(SELECT|INSERT|UPDATE|DELETE|FROM)\b.*`)
+    
 	resss := regexp.MustCompile(`[\s\r\n]+`)
-	sql := resss.ReplaceAllString(string(payloadStr), " ")
-// 	fmt.Println(sql)
-	matches := re.FindAllString(sql, -1)
-	for _, match := range matches {
-	    // 去除空白字符和注释
-        sql = strings.TrimSpace(match)
-        //中文乱码
-        sql = fixChineseEncoding(sql)
-        sql = strings.ReplaceAll(sql, "@", "")
-        
-		verboseStr := fmt.Sprintf("From %s To %s;  %s \n\n", client, server, sql)
-// 		Log.Infof("sql: \n", verboseStr)
-		log.Print(verboseStr)
-	}
+	sql := resss.ReplaceAllString(string(payloadStr[:n]), " ")
+    //中文乱码
+    sql = fixChineseEncoding(sql)
+    sql = strings.ReplaceAll(sql, "@", "")
+    sql = strings.ReplaceAll(sql, "�", "")
+    sql = strings.TrimSpace(sql)
+    
+    re := regexp.MustCompile(`(?i)\b(SELECT|INSERT|UPDATE|DELETE)\b.*`)
+    matches := re.FindAllString(sql, -1)
+    // fmt.Println(matches)
+    if len(matches)>0 {
+        verboseStr := fmt.Sprintf("From %s To %s;  %v \n\n", client, server, sql)
+    	fmt.Println(verboseStr)
+    }
+
+// 	re := regexp.MustCompile(`(?i)\b(SELECT|INSERT|UPDATE|DELETE|\s+|FROM)\b.*`)
+// 	matches := re.FindAllString(sql, -1)
+// 	for _, match := range matches {
+// 	    sql = strings.TrimSpace(match)
+// 		verboseStr := fmt.Sprintf("From %s To %s;  %s \n\n", client, server, sql)
+// // 		Log.Infof("sql: \n", verboseStr)
+// 		fmt.Print(verboseStr)
+// 	}
 
 	return ""
 }
