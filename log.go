109daccc680dad186018a188537221f0c5e1e13f
谢大大 <1130107545@qq.com>
2024-05-23 15:30:09 +0800

    Update log.go

--- log.go
+++ log.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"os"
-
+	"gopkg.in/natefinch/lumberjack.v2"
 	"github.com/sirupsen/logrus"
 )
 
@@ -24,6 +24,20 @@ func initLogger() error {
 		Level:     level,
 		Formatter: new(logrus.JSONFormatter),
 	}
-	Log.Infof("InitLogger: path: %s, level: %s, formatter: json", Config.Log.Path, level)
+	
+	
+    //日志
+    Logs.SetFormatter(&logrus.JSONFormatter{})
+    
+	logger := &lumberjack.Logger{
+		Filename:   "logs/logrus.log",
+		MaxSize:    50,  // 日志文件大小，单位是 MB
+		MaxBackups: 3,    // 最大过期日志保留个数
+		MaxAge:     30,   // 保留过期文件最大时间，单位 天
+		Compress:   true, // 是否压缩日志，默认是不压缩。这里设置为true，压缩日志
+	}
+	Logs.SetOutput(logger) // logrus 设置日志的输出方式
+	
+	
 	return nil
 }
